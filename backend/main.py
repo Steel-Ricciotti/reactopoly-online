ea91e0fd719173964f552cb9e9ab2e42b5c1a68c
Steel Ricciotti <steel.ricciotti@gmail.com>
2025-06-06 17:08:29 -0400

    Tweaked main file

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,12 @@ import uvicorn
 import socketio
 import os
 from socket_handlers import register_socket_handlers
-
+from fastapi.staticfiles import StaticFiles
 # Create FastAPI app and Socket.IO server
 app = FastAPI()
 sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
 asgi_app = socketio.ASGIApp(sio, other_asgi_app=app)
-
+app.mount("/", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "public"), html=True), name="static")
 # Register our event handlers (in socket_handlers.py)
 register_socket_handlers(sio)
 
